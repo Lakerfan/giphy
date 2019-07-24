3e8804f2974d3bcefca5d1b43dedcde95499aa76
Derek Bennett <bnnder@netscape.net>
2019-07-23 17:48:41 -0700

    created the variable actions of my favorite subjects

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    // An array of actions, new actions will be pushed into this array;
+    var actions = ["NBA", "Lakers", "NFL", "Patriots", "Rams", "guitar", "drawing", "painting", "gaming", "war robots","world of warcraft"];
+    
+});
